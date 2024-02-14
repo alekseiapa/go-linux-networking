d64016f3e0b55c5a905b4abb365243a4e653e765
Aleksei Apaseev <aleksei.apaseev@espressif.com>
2024-02-14 09:39:34 +0800

    Data link Layer: Add docker-compose file

--- data-link-layer-practice/receiver/main.go
+++ data-link-layer-practice/receiver/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
@@ -11,51 +10,53 @@ import (
 )
 
 func main() {
-
 	if len(os.Args) != 2 {
-		log.Fatalf("Usage: go run main.go <receiver device name>")
+		log.Fatalf("Usage: go run main.go <receiver MAC address>")
 	}
-	providedDeviceName := os.Args[1]
+	providedMACAddress := os.Args[1]
 
-	devices, err := pcap.FindAllDevs()
+	interfaces, err := net.Interfaces()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var deviceFound bool = false
-	for _, device := range devices {
-		if device.Name == providedDeviceName {
-			deviceFound = true
+	var interfaceName string
+	for _, iface := range interfaces {
+		log.Printf("Checking interface: %s with MAC address %s\n", iface.Name, iface.HardwareAddr.String())
+		if iface.HardwareAddr.String() == providedMACAddress {
+			interfaceName = iface.Name
 			break
 		}
 	}
-	if !deviceFound {
-		log.Fatalf("Device '%s' not found", providedDeviceName)
+
+	if interfaceName == "" {
+		log.Fatalf("No network interface found with MAC address '%s'", providedMACAddress)
 	}
-	listenInterface, _ := net.InterfaceByName(providedDeviceName)
-	listenInterfaceMACAddress := listenInterface.HardwareAddr.String()
-	handle, err := pcap.OpenLive(providedDeviceName, 1500, false, pcap.BlockForever)
+
+	handle, err := pcap.OpenLive(interfaceName, 1500, false, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer handle.Close()
+	log.Printf("Listening for frames on interface %s ...\n", interfaceName)
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
+
 		ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
 		if ethernetLayer == nil {
 			continue
 		}
 		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
 
-		if ethernetPacket.DstMAC.String() != listenInterfaceMACAddress {
+		if ethernetPacket.DstMAC.String() != providedMACAddress {
 			continue
 		}
-		fmt.Println("Reading packet...")
+		log.Println("Received ethernet packet...")
+		log.Println("Reading packet...")
 
-		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
-		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
-		fmt.Println("Payload: ", string(ethernetPacket.Payload))
+		log.Println("Source MAC: ", ethernetPacket.SrcMAC.String())
+		log.Println("Destination MAC: ", ethernetPacket.DstMAC.String())
+		log.Println("Payload: ", string(ethernetPacket.Payload))
 	}
-
 }
--- data-link-layer-practice/sender/main.go
+++ data-link-layer-practice/sender/main.go
@@ -19,8 +19,8 @@ func (f *EthernetFrame) Marshal() []byte {
 	// Ethernet frame format:
 	// | Destination MAC (6 bytes) | Source MAC (6 bytes) | EtherType (2 bytes) | Payload (variable) |
 	frame := make([]byte, 14+len(f.Payload))
-	copy(frame[0:6], f.Destination[0:6])
-	copy(frame[6:12], f.Source[0:6])
+	copy(frame[0:6], f.Destination)
+	copy(frame[6:12], f.Source)
 	frame[12] = byte(f.EtherType >> 8)
 	frame[13] = byte(f.EtherType & 0xff)
 	copy(frame[14:], f.Payload)
@@ -29,32 +29,55 @@ func (f *EthernetFrame) Marshal() []byte {
 
 func main() {
 	if len(os.Args) < 3 {
-		log.Fatal("Usage: go run main.go <src device name> <dst device name>")
+		log.Fatal("Usage: go run main.go <src MAC address> <dst MAC address>")
 	}
-	ifiSrc, err := net.InterfaceByName(os.Args[1])
+	srcMAC, err := net.ParseMAC(os.Args[1])
 	if err != nil {
-		log.Fatalf("could not find source interface %s: %v", ifiSrc, err)
+		log.Fatalf("could not parse source MAC address %s: %v", os.Args[1], err)
 	}
-	ifiDst, err := net.InterfaceByName(os.Args[2])
+	dstMAC, err := net.ParseMAC(os.Args[2])
 	if err != nil {
-		log.Fatalf("could not fidnd destination interface %s: %v", ifiDst, err)
+		log.Fatalf("could not parse destination MAC address %s: %v", os.Args[2], err)
 	}
 
-	conn, err := raw.ListenPacket(ifiSrc, 0x0800, nil)
+	// Select a network interface to send the frame
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Available network interfaces: ", interfaces)
+	var ifi *net.Interface
+	for _, iface := range interfaces {
+		log.Printf("Checking interface: %s with MAC address %s\n", iface.Name, iface.HardwareAddr.String())
+		if iface.Flags&net.FlagUp != 0 && iface.Flags&net.FlagLoopback == 0 {
+			if iface.HardwareAddr.String() == srcMAC.String() {
+				ifi = &iface
+				log.Printf("Selected network interface: %s\n", iface.Name)
+				break
+			}
+		}
+	}
+	if ifi == nil {
+		log.Fatal("no suitable interface found")
+	}
+
+	conn, err := raw.ListenPacket(ifi, 0x0800, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
 	payload := []byte("Hi!")
 
 	frame := &EthernetFrame{
-		Destination: ifiDst.HardwareAddr,
-		Source:      ifiSrc.HardwareAddr,
+		Destination: dstMAC,
+		Source:      srcMAC,
 		EtherType:   0x0800,
 		Payload:     payload,
 	}
+
 	log.Printf("Sending frame from %s to %s with payload: %s\n", frame.Source, frame.Destination, hex.EncodeToString(frame.Payload))
-	if _, err = conn.WriteTo(frame.Marshal(), &raw.Addr{HardwareAddr: ifiDst.HardwareAddr}); err != nil {
+	if _, err = conn.WriteTo(frame.Marshal(), &raw.Addr{HardwareAddr: dstMAC}); err != nil {
 		log.Fatal(err)
 	}
 }
